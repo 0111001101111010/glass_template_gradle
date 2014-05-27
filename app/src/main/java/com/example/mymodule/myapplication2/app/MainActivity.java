ed30b0362f9ab4f34188014e34dc62fe8b40050b
stanzheng <dev.stanzheng@gmail.com>
2014-05-27 16:33:44 -0400

    gradle boiler plate

--- app/src/main/java/com/example/mymodule/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/mymodule/myapplication2/app/MainActivity.java
@@ -4,14 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.google.android.glass.app.Card;
 
+import android.view.View;
+import android.view.WindowManager;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+
+        Card card1 = new Card(this);
+        card1.setText("Main Card Body!");
+        card1.setFootnote("Footer");
+        View card1View = card1.getView();
+        setContentView(card1View);
     }
 
 
