92dd73625e1211eff23b590607da9c718e80bfd9
stanzheng <dev.stanzheng@gmail.com>
2014-05-28 11:10:56 -0400

    adding gesture detection

--- app/src/main/java/com/example/mymodule/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/mymodule/myapplication2/app/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.mymodule.myapplication2.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import com.google.android.glass.app.Card;
-
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 
+import com.google.android.glass.app.Card;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+
 public class MainActivity extends Activity {
 
+    GestureDetector mGestureDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +49,68 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private GestureDetector createGestureDetector(Context context) {
+        GestureDetector gestureDetector = new GestureDetector(context);
+        //Create a base listener for generic gestures
+        gestureDetector.setBaseListener( new GestureDetector.BaseListener() {
+            @Override
+            public boolean onGesture(Gesture gesture) {
+                if (gesture == Gesture.LONG_PRESS ) {
+                    Log.d("@@@@", "TAP");
+                    //	displaySpeechRecognizer();
+                    openOptionsMenu();
+                    //card.setText("TAP!");
+                    //card.setImageLayout(Card.ImageLayout.FULL);
+                    //View card1View = card.toView();
+                    //setContentView(card1View);
+                    // do something on tap
+                    return true;
+                } else if (gesture == Gesture.TWO_TAP) {
+                    // do something on two finger tap
+                    //scanBarcode();
+                    //maybe pass some context; not important
+//                    Intent i = new Intent(getApplicationContext(), ScanditSDKDemoSimple.class);
+//                    i.putExtra("new_variable_name","value");
+//                    startActivityForResult(i, SCANDIT_CODE_REQUEST);
+//                    Log.d("@@@@", "2-TAP");
+                    return true;
+                } else if (gesture == Gesture.TAP) {
+                    // do something on right (forward) swipe
+                    Log.d("@@@@", "2-TAP");
+                    openOptionsMenu();
+
+                    return true;
+                }
+                return false;
+            }
+        });
+        gestureDetector.setFingerListener(new GestureDetector.FingerListener() {
+            @Override
+            public void onFingerCountChanged(int previousCount, int currentCount) {
+                // do something on finger count changes
+            }
+        });
+        gestureDetector.setScrollListener(new GestureDetector.ScrollListener() {
+            @Override
+            public boolean onScroll(float displacement, float delta, float velocity) {
+
+                return true;
+                // do something on scrolling
+            }
+        });
+        return gestureDetector;
+    }
+
+    /*
+     * Send generic motion events to the gesture detector
+     */
+    @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if (mGestureDetector != null) {
+            return mGestureDetector.onMotionEvent(event);
+        }
+        return false;
     }
 }
